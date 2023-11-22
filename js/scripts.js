913800a6a9f750699f4caf59eabf44ebe4f6c987
Kosorotikov Anton <darkringfire@gmail.com>
2023-11-22 11:16:27 +0300

    Add menu links color by scroll

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,21 @@ document.addEventListener('DOMContentLoaded', function() {
     } else {
       navbarCollapsible.classList.add('navbar-shrink');
     }
+
+    const links = document.querySelectorAll('.nav-link');
+    const sections = document.querySelectorAll('section');
+
+    sections.forEach((section) => {
+      if (window.scrollY >= section.offsetTop) {
+        // console.log(window.scrollY + ' >= ' + section.offsetTop + ' #' + section.id);
+        links.forEach((link) => {
+          link.classList.remove('active');
+          if (link.hash === '#' + section.id) {
+            link.classList.add('active');
+          }
+        });
+      }
+    });
   }
 
   navInit();
